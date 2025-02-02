bd7dbdbc269ee04dd8dad812997a02dd28c41ca8
Nicolas Gajardo <nicolasgajardo11@gmail.com>
2025-02-02 02:54:29 -0300

    mini avance

--- devtree/src/index.ts
+++ devtree/src/index.ts
@@ -1,5 +1,10 @@
-//Importa el módulo Express
-const express = require('express') 
+//-----Importa el módulo Express----
+//   sintaxis CJS
+//const express = require('express')// Common JS
+
+//   sintaxis ESM
+import express from 'express'// Ecmascript modules
+//-------------------------
 
 //Para ver lo que se obtiene de Express por consola
 //console.log(express)
@@ -12,7 +17,7 @@ const app = express()
 //como por ejemplo el navegador que se está usando, el SO, y a veces datos de formulario) 
 //y se recibe un res (Se muestran productos de una p´gina web, como productos)
 app.get('/', (request, response) => {
-    response.send('Hola mundo en Express')
+    response.send('Hola mundo en Express / TypeScript')
 })
 
 app.get('/ecommerce', (request, response) => {
@@ -24,10 +29,11 @@ app.get('/blog', (request, response) => {
 })
 //-------------
 
-
+// proccess.env.PORT dice "utiliza esa variable de entorno llamada PORT si existe". sino existe se usa el 4000
+const port = process.env.PORT || 4000 // esto se define solo antes de hacer el hacer el deployment, después el hosting define el puerto
 //Creamos el servidor. El puerto es el 4000
-app.listen(4000, () => {
-    console.log('Servidor funcionando...')
+app.listen(port, () => {
+    console.log('Servidor funcionando en el puerto:', port)
 })
 
 
